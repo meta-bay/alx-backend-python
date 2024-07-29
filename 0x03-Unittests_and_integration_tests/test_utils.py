81615a0839f3b531f5cfc24506cfeb75c6a52a8b
meta-bay <bayiemeseret@gmail.com>
2024-07-29 19:05:44 +0300

    Mock HTTP calls

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -3,6 +3,7 @@
 test utils
 '''
 import unittest
+from unittest.mock import patch, Mock
 from parameterized import parameterized
 import utils
 
@@ -26,3 +27,20 @@ class TestAccessNestedMap(unittest.TestCase):
         '''tests if it raises keyerror as it should'''
         with self.assertRaises(expected):
             utils.access_nested_map(nested_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+    '''class to test get_json function'''
+
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+    @patch('utils.requests.get')
+    def test_get_json(self, test_url, test_payload, req):
+        '''tests get_json function'''
+        mock = Mock()
+        mock.json.return_value = test_payload
+        req.return_value = mock
+        self.assertEqual(utils.get_json(test_url), test_payload)
+        req.assert_called_once_with(test_url)
